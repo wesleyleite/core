a18468cbeb87ff228e4229cd7cb9d2412600fe16
wesleyleite <wesleyhenrique@gmail.com>
2015-02-02 22:19:51 -0200

    added funcoes to cpus and net resource

--- core/cpus/__init__.py
+++ core/cpus/__init__.py
@@ -1,6 +1,7 @@
 # *-* coding: utf-8 *-*
 from flask import Blueprint, jsonify
-import psutil
+from psutil import NUM_CPUS, cpu_count
+from funcoes import usage_percent
 
 cpu = Blueprint('cpu', __name__, 
                     template_folder='')
@@ -8,4 +9,9 @@ cpu = Blueprint('cpu', __name__,
 @cpu.route('/cpu/num', methods=['GET'])
 def cpu_num():
     return jsonify(CPU=psutil.NUM_CPUS, 
-            NOLOGICAL=psutil.cpu_count(logical=False))
+            NO_LOGICAL=psutil.cpu_count(logical=False))
+
+@cpu.route('/cpu/usage/<int:trange>', methods=['GET'])
+@cpu.route('/cpu/usage', methods=['GET'])
+def cpu_percent_usage(trange=3):
+    return jsonify(CPU_USAGE=usage_percent(trange))
--- core/nets/__init__.py
+++ core/nets/__init__.py
@@ -7,8 +7,17 @@ net = Blueprint('net', __name__,
 
 @net.route('/net/iface')
 @net.route('/net/iface/<interface>', methods=['GET'])
-def ifaces(interface):
-    return jsonify(psutil.net_io_counters(pernic=True))
+def ifaces(interface=all):
+    lIface=[]
+    dIface=psutil.net_io_counters(pernic=True)
+    for v,k in enumerate(dIface):
+        lIface.append(k)
+
+    if interface in lIface:
+        rIface=dIface[interface]
+    else:
+        rIface=lIface
+    return jsonify(interface=rIface)
 
 @net.route('/net/connections', methods=['GET'])
 def connections():
