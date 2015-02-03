84367c06c455a8a313aaec5df5c251dcc9acfa41
wesleyleite <wesleyhenrique@gmail.com>
2015-02-02 22:36:43 -0200

    adjust load module

--- core/load_modules.sh
+++ core/load_modules.sh
@@ -3,11 +3,12 @@
 module_dir="$1"
 blueprint_name="$2"
 
+echo $module_dir $blueprint_name
+
 [ -e "$module_dir" -a \
-	! -z "$blueprint_name" ] && 
-{
-	echo "app.register_blueprint($blueprint_name)" #>> blue_register.py
-	echo "from $module_dir from $blueprint_name" #>> imports.py
+	! -z "$blueprint_name" ] && {
+	echo "app.register_blueprint($blueprint_name)" >> blue_register.py
+	echo "from $module_dir from $blueprint_name" >> imports.py
 } || {
 	echo "$0 MODULE_DIR_NAME BLUEPRINT_NAME"
 }
