a7da3a0782066bacc5f4910257a15fc184a62eb0
wesleyleite <wesleyhenrique@gmail.com>
2015-02-12 19:02:22 -0200

    added module disk

---
+++ core/disks/__init__.py
@@ -0,0 +1,38 @@
+# *-* coding: utf-8 *-*
+from flask import Blueprint, jsonify
+from psutil import disk_partitions, disk_usage
+from ..auths import *
+
+disk = Blueprint('disk', __name__, 
+                    template_folder='')
+
+@disk.route('/disk', methods=['GET'])
+@auth.login_required
+def disk_part():
+    ldisk = []
+    fdisk = disk_partitions(all=False)
+    for n,dd in enumerate(fdisk):
+        ldisk.append({
+            'id': n,
+            'device': dd.device,
+            'mountpoint': dd.mountpoint,
+            'fstype': dd.fstype,
+            'opts': dd.opts})
+
+    return jsonify(disk=ldisk)
+
+@disk.route('/disk/usage/<int:id>', methods=['GET'])
+@auth.login_required
+def disk_use(id):
+    ldisk = []
+    try:
+        diskpath = disk_partitions(all=False)[id]
+        dd = disk_usage(diskpath[1])
+        ldisk.append({
+            'total': dd.total,
+            'used': dd.used,
+            'free': dd.free,
+            'percent': dd.percent})
+    except:
+        ldisk=None
+    return jsonify(diskusage=ldisk)
--- core/imports.py
+++ core/imports.py
@@ -4,3 +4,4 @@ from oss import os
 from users import user
 from process import process
 from mems import mem 
+from disks import disk 
