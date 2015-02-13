903fa6f32b9663dc134c71afdddb0a9954211d5c
wesleyleite <wesleyhenrique@gmail.com>
2015-02-13 00:01:15 -0200

    added /cpu/info get more info cpu

--- core/cpus/__init__.py
+++ core/cpus/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify
 from psutil import NUM_CPUS, cpu_count
 from funcoes import usage_percent
 from ..auths import *
+from cpuinfo import cpuinfo
 
 cpu = Blueprint('cpu', __name__, 
                     template_folder='')
@@ -10,11 +11,16 @@ cpu = Blueprint('cpu', __name__,
 @cpu.route('/cpu/num', methods=['GET'])
 @auth.login_required
 def cpu_num():
-    return jsonify(CPU=psutil.NUM_CPUS, 
-            NO_LOGICAL=psutil.cpu_count(logical=False))
+    return jsonify(CPU=cpu_count(), 
+            NO_LOGICAL=cpu_count(logical=False))
 
 @cpu.route('/cpu/usage/<int:trange>', methods=['GET'])
 @cpu.route('/cpu/usage', methods=['GET'])
 @auth.login_required
 def cpu_percent_usage(trange=3):
     return jsonify(CPU_USAGE=usage_percent(trange))
+
+@cpu.route('/cpu/info', methods=['GET'])
+@auth.login_required
+def cpu_info():
+    return jsonify(cpuinfo=cpuinfo.get_cpu_info())
